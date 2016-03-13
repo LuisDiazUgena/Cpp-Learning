f11891c31fac1389954dcb21b15850fde52490e9
luisdiazugena <luisdiazugena@gmail.com>
2016-03-13 23:52:27 +0100

    Added class template folder and files

---
+++ thenewboston/classTemplate/classFile.cpp
@@ -0,0 +1,5 @@
+#include "classFile.h"
+
+classFile::classFile(){
+  
+}
---
+++ thenewboston/classTemplate/classFile.h
@@ -0,0 +1,10 @@
+#ifndef CLASSFILE_H
+#define CLASSFILE_H
+
+class classFile{
+    private:
+
+    public:
+}
+
+#endif //CLASSFILE_H
